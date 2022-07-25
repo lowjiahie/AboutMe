69e78a44e55f91a767b5f780ebc5d94cd9581cd6
LowJiaHie <jiahielow8@gmail.com>
2022-07-26 00:27:20 +0800

    AboutMe-V2

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,30 +8,70 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        //creating a binding object that contains a reference to each view
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        /**
+         * In onCreate(), set the value of the myName variable in the layout file to
+         * the value of the myName variable that you just declared.
+         * You can't access the variable in the XML directly.
+         * You need to access it through the binding object.
+         */
+        binding.myName = myName
+        /**
+         *  When the binding object is created,
+         *  the compiler generates the names of the views in the binding object from the IDs of the views in the layout,
+         *  converting them to camel case. So, for example, done_button is doneButton in the binding object,
+         *  nickname_edit becomes becomes nicknameEdit, and nickname_text becomes nicknameText.
+         */
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
 
     }
 
     private fun addNickname(view : View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        /**
+         * The nicknameText requires a String, and nicknameEdit.text is an Editable.
+         * When using data binding, it is necessary to explicitly convert the Editable to a String.
+         */
+        //First ways
+//        binding.nicknameText.text = binding.nicknameEdit.text.toString()
+//        binding.nicknameEdit.visibility = View.GONE
+//        binding.doneButton.visibility = View.GONE
+//        binding.nicknameText.visibility = View.VISIBLE
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        //second ways
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            /**
+             * After set the nickname, to refresh the UI with the new data.
+             * Therefore, invalidate all binding expressions so that they are recreated with correct data
+             */
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
+
+    /**
+     * Tip: If you see compiler errors after you make changes,
+     * select Build > Clean Project followed by Build > Rebuild Project.
+     * Doing this usually updates the generated files.
+     * Otherwise, select File > Invalidate Caches/Restart to do a more thorough cleanup.
+     */
 }
